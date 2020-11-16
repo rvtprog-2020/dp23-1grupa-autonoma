a6c010038a0852e03ddb42f9e86eb2e3d57ccc83
Maxim Cvetkov <maximpixel@yandex.ru>
2020-11-16 16:31:03 +0200

    Move pages

--- main.py
+++ main.py
@@ -26,9 +26,29 @@ def car_by_id(id):
         if id < 0 or id >= len(cars):
             raise Exception("Out of index")
 
-        return render_template("car.html", car=cars[id])
+        return render_template("car_info.html", car=cars[id])
     except:
         return "404 car not found"
+
+@app.route("/cars")
+def cars_list():
+    return render_template("cars_list.html")
+
+@app.route("/car/0/reserve")
+def car_reserve():
+    return render_template("car_reserve.html")
+
+@app.route("/reserved")
+def reserved_cars_list():
+    return render_template("reserved_cars_list.html")
+
+@app.route("/admin/cars")
+def admin_cars_list():
+    return render_template("admin/cars_list.html")
+
+@app.route("/admin/car/0/edit")
+def admin_car_edit():
+    return render_template("admin/car_edit.html")
 
 if __name__ == '__main__':
     app.run(port=80, debug=True)
