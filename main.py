c1322cdc375701fc21f71761ff37027fb06a1d19
Maxim Cvetkov <maximpixel@yandex.ru>
2020-11-16 15:45:35 +0200

    Fix

--- main.py
+++ main.py
@@ -6,4 +6,5 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-app.run(host="0.0.0.0",port=80, debug=True)
+if __name__ == '__main__':
+    app.run(port=80, debug=True)
