4c705b1afc6f4fd41d568c5ac18f69aa13e649fe
Maxim Cvetkov <maximpixel@yandex.ru>
2020-12-07 14:02:14 +0200

    ????

--- mongodb_test.py
+++ mongodb_test.py
@@ -7,19 +7,23 @@ def run():
     password = "ZeP-iFc-jwZ-q46"
     dbname = "cluster0"
 
-    client = MongoClient("mongodb+srv://boss:" + password + "@cluster0.839ly.mongodb.net/" + dbname + "?retryWrites=true&w=majority")
+    client = MongoClient("mongodb://boss:" + password + "@cluster0.839ly.mongodb.net/" + dbname + "?retryWrites=true&w=majority")
 
+    print("\nClient:")
     print(client)
+
+    db = client['cluster0']
+
+    print("\nDB:")
+    print(db)
+
+    users_db = db["users"]
+
+    print("\nUsers DB:")
+    print(users_db)
+
+    # SOS
+    users_db.insert_one({"name": "Kek", "surname": "Petrovich", "password": "qwerty12345"})
 
 if __name__ == "__main__":
     run()
-
-# db = client["cluster0"]
-
-# print(db)
-
-# users_db = db.users
-
-# users_db.insert_one({"name": "Kek", "surname": "Petrovich", "password": "qwerty12345"})
-
-#client = MongoClient("mongodb+srv://maris:famN3CBh7Wvtc6Pj@flaskproject.3itkl.mongodb.net/myproject?retryWrites=true&w=majority")
