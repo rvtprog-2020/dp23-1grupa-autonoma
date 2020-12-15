242210eff372e6356255a714f59686c51b22b8b7
Maxim Cvetkov <maximpixel@yandex.ru>
2020-12-15 09:48:13 +0200

    Db checker

--- mongodb_test.py
+++ mongodb_test.py
@@ -13,7 +13,7 @@ def connect_db(dbname):
     
     return MongoClient("mongodb+srv://" + username + ":" + password + "@" + cluster_name + ".839ly.mongodb.net/" + dbname + "?retryWrites=true&w=majority")
 
-def run():
+def run_brand_fixer():
     client = connect_db("test")
 
     db = client["test"]
@@ -32,5 +32,38 @@ def run():
 
         })
 
+def check_car(car):
+    return "name" in car and "image" in car and "mileage" in car and "seats" in car and "price" in car and "rent_point_id" in car and "brand" in car
+
+def check_reservation(reservation):
+    return "car_id" in reservation and "days" in reservation and "price" in reservation and "date" in reservation
+
+def check_rent_point(rent_point):
+    return "name" in rent_point and "location" in rent_point and "image" in rent_point
+
+def run_db_check():
+    client = connect_db("test")
+    db = client["test"]
+    cars_db = db["cars"]
+    reservations_db = db["reservations"]
+    rent_points_db = db["rent_points"]
+
+    for car in cars_db.find():
+        if not check_car(car):
+            print("Check failed car", car)
+            exit()
+    
+    for reservation in reservations_db.find():
+        if not check_reservation(reservation):
+            print("Check failed reservation", reservation)
+            exit()
+    
+    for rent_point in rent_points_db.find():
+        if not check_rent_point(rent_point):
+            print("Check failed rent point", rent_point)
+            exit()
+    
+    print("Successfully checked!")
+
 if __name__ == "__main__":
-    run()
+    run_db_check()
